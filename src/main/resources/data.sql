096a2bc62c3ea324d2d9de4e768504d3e4de4ef8
LeandroPalma27 <psychomusicbox@gmail.com>
2022-11-05 21:49:02 -0500

    Correcciones

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO `genders_user` (`id`, `gender`) VALUES
+INSERT IGNORE INTO `genders_user` (`id`, `gender`) VALUES
 (1, 'MALE'),
 (2, 'FEMALE'),
 (3, 'NOT_SPECIFY');
 
-INSERT INTO `roles_user` (`id`, `authority`) VALUES
+INSERT IGNORE INTO `roles_user` (`id`, `authority`) VALUES
 (1, 'ROLE_ADMIN'),
 (2, 'ROLE_USER');
